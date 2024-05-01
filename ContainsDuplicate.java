01c454d18d7ff60e3fae4f43edd119464df02253
[Ujjawala Katakwar] <ujjawala1501@gmail.com>
2024-05-01 15:21:59 +0530

    Array_Hashmap_Sort_Easy

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -13,6 +13,7 @@ Example 3:
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
  */
+/*
 import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
@@ -28,8 +29,26 @@ class Solution {
         return false;
     }
 }
+*/
+//Alternate Solution
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int size = nums.length;
+        HashMap<Integer,Integer> countNum = new HashMap<Integer,Integer> ();
+        for (int i=0;i<size ; i++){
+            if(!countNum.containsKey(nums[i])){
+                countNum.put(nums[i],1);
+            }
+            else{
+                countNum.put(nums[i],countNum.get(nums[i])+1);
+            }
 
-public static void main (String[] args) throws Exception {
-    Solution s = new Solution();
-    s.containsDuplicate([1,1,1,3,3,4,3,2,4,2]);
+        }
+        for(int j=0;j<size;j++){
+            if(countNum.get(nums[j])>1){
+                return true;
+            }
+        }
+        return false;
     }
+}
