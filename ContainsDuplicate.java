9b2f94df8ff7f9d3472274c5bbfbb7ed99faaced
[Ujjawala Katakwar] <ujjawala1501@gmail.com>
2024-05-01 14:40:33 +0530

    Array_Sort_Easy

---
+++ ContainsDuplicate.java
@@ -0,0 +1,35 @@
+/*
+ Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ */
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int size = nums.length;
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i =1; i < size;i++){
+            if(nums[i] == nums[i-1]){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+}
+
+public static void main (String[] args) throws Exception {
+    Solution s = new Solution();
+    s.containsDuplicate([1,1,1,3,3,4,3,2,4,2]);
+    }
