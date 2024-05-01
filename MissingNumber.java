1d0fe2e498c5c83684e68505171261b0b11d15df
[Ujjawala Katakwar] <ujjawala1501@gmail.com>
2024-05-01 11:27:20 +0530

    Array_Easy

---
+++ MissingNumber.java
@@ -0,0 +1,30 @@
+/*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums. */
+
+import java.util.*;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> countNumMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            countNumMap.put(nums[i], 1);
+        }
+        for (int j = 0; j <= n; j++) {
+            if (countNumMap.get(j) == null) {
+                return j;
+            }
+        }
+        return -1; // No missing number found
+    }
+}
